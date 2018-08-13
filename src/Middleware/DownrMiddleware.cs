f57591ff19e0757b685954e27e9dd9bc710c1415
Ivan Ivanov <csyntax@outlook.com>
2018-08-13 13:52:34 +0300

    rename DownMiddleware :+1:

--- src/Middleware/DownrMiddleware.cs
+++ src/Middleware/DownrMiddleware.cs
@@ -12,25 +12,35 @@
     using Microsoft.AspNetCore.StaticFiles;
     using Microsoft.AspNetCore.ResponseCompression;
 
-    using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.WebEncoders;
     using Microsoft.Extensions.FileProviders;
     using Microsoft.Extensions.DependencyInjection;
 
     using downr.Services;
+    using downr.Services.Posts;
 
-    public static class DownrMiddlewareExtensions
+    public static class DownrMiddleware
     {
+        private static string[] mimeTypes = new string[]
+        {
+            "text/plain",
+            "text/css",
+            "text/html",
+            "image/svg+xml",
+            "application/javascript"
+        };
+        
         public static void AddDownr(this IServiceCollection services)
         {
-            string[] mimeTypes = new string[]
-            {
-                "text/plain",
-                "text/css",
-                "text/html",
-                "image/svg+xml",
-                "application/javascript"
-            };
+            services
+              .AddMvc()
+              .AddMvcLocalization()
+              .AddRazorPagesOptions(options =>
+              {
+                  options.Conventions.AddPageRoute("/Index", "/Posts");
+                  options.Conventions.AddPageRoute("/Post", "/Posts/{slug}");
+                  options.Conventions.AddPageRoute("/Category", "/Categories/{name}");
+              });
 
             var textEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
 
@@ -46,19 +56,9 @@
 
             services.Configure<WebEncoderOptions>(options => options.TextEncoderSettings = textEncoderSettings);
 
-            services
-               .AddMvc()
-               .AddMvcLocalization()
-               .AddRazorPagesOptions(options =>
-               {
-                   options.Conventions.AddPageRoute("/Index", "/Posts");
-                   options.Conventions.AddPageRoute("/Post", "/Posts/{slug}");
-                   options.Conventions.AddPageRoute("/Category", "/Categories/{name}");
-               })
-               .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
-
             services.AddSingleton<IMarkdownContentLoader, MarkdownContentLoader>();
             services.AddSingleton<IYamlIndexer, YamlIndexer>();
+            services.AddScoped<IPostService, PostService>();
         }
 
         public static void UseDownr(this IApplicationBuilder app, IHostingEnvironment env)
@@ -66,14 +66,7 @@
             if (string.IsNullOrWhiteSpace(env.WebRootPath))
             {
                 env.WebRootPath = Constants.WebRootPath;
-            }            
-
-            var provider = new FileExtensionContentTypeProvider();
-
-            provider.Mappings.Remove(".md");
-            provider.Mappings.Remove(".js");
-            provider.Mappings.Remove(".css");
-
+            }
 
             if (env.IsDevelopment())
             {
@@ -84,11 +77,13 @@
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseMvc();
-            app.UseStaticFiles();
-            app.UseResponseCompression();
+            var provider = new FileExtensionContentTypeProvider();
 
-            app.UseStaticFiles(new StaticFileOptions
+            provider.Mappings.Remove(".md");
+            provider.Mappings.Remove(".js");
+            provider.Mappings.Remove(".css");
+
+            var staticFileOptions = new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Constants.ContentPath),
                 RequestPath = "/posts",
@@ -97,7 +92,13 @@
                 {
                     ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
                 }
-            });
+            };
+
+            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseResponseCompression();
+
+            app.UseStaticFiles(staticFileOptions);         
 
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
