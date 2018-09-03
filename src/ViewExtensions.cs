97bacf97f3d9a8d1f7c839421fb6bcc9cddf0b89
Ivan Ivanov <csyntax@outlook.com>
2018-09-03 12:08:39 +0300

    create view extensions

---
+++ src/ViewExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace downr
+{
+    using System;
+    using System.Linq;
+
+    using HtmlAgilityPack;
+
+    using Microsoft.AspNetCore.Html;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+   
+    public static class ViewExtensions
+    {
+        public static IHtmlContent Truncate(this IHtmlHelper helper, string content, int snippetLength = 250)
+        {
+            content = content.RemoveHtmlTags();
+
+            if (snippetLength < content.Length)
+            {
+                int index = content.LastIndexOfAny(new char[] { ' ', '\n', 'r', '\t' }, snippetLength);
+
+                if (index < 0)
+                {
+                    index = snippetLength;
+                }
+
+                content = content.Substring(0, index).TrimEnd();
+            }
+            
+            return helper.Raw(content);
+        }
+
+        
+
+        private static string RemoveHtmlTags(this string markup)
+        {
+            if (string.IsNullOrEmpty(markup))
+            {
+                return string.Empty;
+            }
+
+            var document = new HtmlDocument();
+
+            document.LoadHtml(markup);
+
+            document.DocumentNode
+                .Descendants()
+                .Where(n => n.Name == "pre")
+                .ToList()
+                .ForEach(n => n.Remove());
+
+            return document.DocumentNode.InnerText;
+        }
+    }
+}
