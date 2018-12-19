ec17598e2dd11871f546346ee913fb86e9cb0c11
Ivan Ivanov <csyntax@outlook.com>
2018-12-20 00:38:53 +0200

    caching

--- src/Components/CategoryViewComponent.cs
+++ src/Components/CategoryViewComponent.cs
@@ -6,6 +6,7 @@
 
     using downr.Services.Posts;
     using System;
+    using System.Threading.Tasks;
 
     public class CategoryViewComponent : ViewComponent
     {
@@ -18,13 +19,25 @@
             this.postService = postService;
         }
 
-        public IViewComponentResult Invoke()
+        /*public IViewComponentResult Invoke()
         {
             string[] categories = this.memoryCache.GetOrCreate("Categories", entry =>
             {
                 entry.SetSlidingExpiration(TimeSpan.FromDays(1));
 
                 return this.postService.GetCategories();
+            });
+
+            return this.View(categories);
+        }*/
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string[] categories = await this.memoryCache.GetOrCreateAsync("Categories", entry =>
+            {
+                entry.SetSlidingExpiration(TimeSpan.FromDays(1));
+
+                return Task.FromResult(this.postService.GetCategories());
             });
 
             return this.View(categories);
