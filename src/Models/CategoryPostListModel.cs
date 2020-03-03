c89060f5f042bdb6646546d1cb4e84bd2915565e
Ivan Ivanov <csyntax@outlook.com>
2020-03-03 13:12:55 +0200

    clear brady code

--- src/Models/CategoryPostListModel.cs
+++
@@ -1,8 +0,0 @@
-namespace downr.Models
-{
-    public class CategoryPostListModel
-    {
-        public string CategoryName { get; set; }
-        public PostListModel PostList { get; set; }
-    }
-}
